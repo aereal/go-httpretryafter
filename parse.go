cb6838e88469d43a86b9bab830b11fea3261c625
aereal <aereal@aereal.org>
2020-10-02 14:26:18 +0900

    docs: add documentation comments for exported symbols

--- parse.go
+++ parse.go
@@ -7,12 +7,17 @@ import (
 )
 
 var (
+	// ErrNegativeSecondsNotAllowed is parsing error that represents seconds value is negative.
+	// The seconds value in Retry-After must be positive.
 	ErrNegativeSecondsNotAllowed = errors.New("negative seconds not allowed")
-	ErrInvalidFormat             = errors.New("Retry-After value must be seconds integer or HTTP date string")
+
+	// ErrInvalidFormat is parsing error that represents given Retry-After neither valid seconds nor valid HTTP date.
+	ErrInvalidFormat = errors.New("Retry-After value must be seconds integer or HTTP date string")
 
 	nowFunc = func() time.Time { return time.Now() }
 )
 
+// Parse tries to parse the value as seconds or HTTP date.
 func Parse(retryAfter string) (time.Time, error) {
 	if dur, err := ParseSeconds(retryAfter); err == nil {
 		now := nowFunc()
@@ -24,6 +29,7 @@ func Parse(retryAfter string) (time.Time, error) {
 	return time.Time{}, ErrInvalidFormat
 }
 
+// ParseSeconds parses the value as seconds.
 func ParseSeconds(retryAfter string) (time.Duration, error) {
 	seconds, err := strconv.ParseInt(retryAfter, 10, 64)
 	if err != nil {
@@ -35,6 +41,7 @@ func ParseSeconds(retryAfter string) (time.Duration, error) {
 	return time.Second * time.Duration(seconds), nil
 }
 
+// ParseHTTPDate parses the value as HTTP date.
 func ParseHTTPDate(retryAfter string) (time.Time, error) {
 	parsed, err := time.Parse(time.RFC1123, retryAfter)
 	if err != nil {
